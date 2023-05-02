d5b166cae21d118a0c9332e8548439c5a564011a
profbiyi <ahmed.oladapo@gmail.com>
2023-05-02 23:13:19 +0200

    set up terraform instance on aws

---
+++ profider.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "app_server" {
+  ami           = "ami-02396cdd13e9a1257"
+  instance_type = "t3.nano"
+
+  tags = {
+    Name = "Terraform in 21 Days by Vladimir"
+    Owner = "profbiyi"
+  }
+}
